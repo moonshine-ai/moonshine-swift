0ff21bd0ee64f3dbbc3465a49a07222ac6b36c74
Pete Warden <pete@usefulsensors.com>
2026-01-03 13:24:54 -0800

    Release v0.0.28

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
             // Uncomment this to use the locally-built XCFramework
             // path: "Moonshine.xcframework",
             url:
-                "https://github.com/moonshine-ai/moonshine-swift/releases/download/v0.0.27/Moonshine.xcframework.zip",
-            checksum: "293cb68cb2adcf8e097e87a0fe6e45dce3405e641fcc555d7c9a95a461608c3c"
+                "https://github.com/moonshine-ai/moonshine-swift/releases/download/v0.0.28/Moonshine.xcframework.zip",
+            checksum: "cd82b916a990bc32c31eab8be5fce694d5a4fed1ea1e99b5b3e96893b7403796"
         ),
         .target(
             name: "MoonshineVoice",
